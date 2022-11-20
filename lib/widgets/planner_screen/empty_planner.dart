e3adf5826a050f33ecbe264948332d3f5483ea97
mts <mathewsawicki@gmail.com>
2022-11-20 19:16:09 +0100

    empty planner style and turn into card

--- lib/widgets/planner_screen/empty_planner.dart
+++ lib/widgets/planner_screen/empty_planner.dart
@@ -7,33 +7,45 @@ class EmptyPlanner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.only(bottom: 70),
-      constraints: const BoxConstraints.expand(),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Icon(MdiIcons.playlistRemove, size: 250, color: Colors.grey[300]),
-          Text('Brak procedur',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 25, color: Colors.grey[300])),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text('naciśnij ',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 20, color: Colors.grey[300])),
-              Icon(
-                Icons.add_circle,
-                color: Colors.grey[300],
-              ),
-              Text(' aby dodać procedurę',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 20, color: Colors.grey[300])),
-            ],
-          )
-        ],
+      padding: const EdgeInsets.only(bottom: 8, left: 8, right: 8),
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        child: Center(
+          child: Container(
+            padding: const EdgeInsets.only(bottom: 70),
+            constraints: const BoxConstraints.expand(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Icon(MdiIcons.playlistRemove,
+                    size: 250, color: Colors.grey[500]),
+                Text('Brak procedur',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 25, color: Colors.grey[500])),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('naciśnij ',
+                        textAlign: TextAlign.center,
+                        style:
+                            TextStyle(fontSize: 20, color: Colors.grey[500])),
+                    // ignore: prefer_const_constructors
+                    Icon(
+                      Icons.add_circle,
+                      color: Colors.blue,
+                    ),
+                    Text(' aby dodać procedurę',
+                        textAlign: TextAlign.center,
+                        style:
+                            TextStyle(fontSize: 20, color: Colors.grey[500])),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
