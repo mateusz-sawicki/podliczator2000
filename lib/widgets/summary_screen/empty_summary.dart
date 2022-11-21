fa16eb42838ade7b98724acd347f1186593446f7
mts <mathewsawicki@gmail.com>
2022-11-21 14:07:58 +0100

    removed padding in empty summary

--- lib/widgets/summary_screen/empty_summary.dart
+++ lib/widgets/summary_screen/empty_summary.dart
@@ -6,25 +6,21 @@ class EmptySummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 8, left: 8, right: 8),
-      child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-        child: Center(
-          child: Container(
-            padding: const EdgeInsets.only(bottom: 70),
-            constraints: const BoxConstraints.expand(),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Icon(MdiIcons.playlistRemove,
-                    size: 250, color: Colors.grey[500]),
-                Text('Brak procedur',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 25, color: Colors.grey[500])),
-              ],
-            ),
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: Center(
+        child: Container(
+          padding: const EdgeInsets.only(bottom: 70),
+          constraints: const BoxConstraints.expand(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Icon(MdiIcons.playlistRemove, size: 250, color: Colors.grey[500]),
+              Text('Brak procedur',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 25, color: Colors.grey[500])),
+            ],
           ),
         ),
       ),
